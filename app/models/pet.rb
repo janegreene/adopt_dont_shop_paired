8f94b17b08022f71dd1e1c05a5367eb3519db88e
Jane Greene <janegreenecasa@gmail.com>
2020-05-14 14:03:58 -0600

    added relationship tests from pet applications and pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
   validates_presence_of :name
   belongs_to :shelter
+  has_many :pet_applications
+  has_many :pets, through: :pet_applications
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Pet do
 
   describe 'relationships' do
     it {should belong_to :shelter}
+    it {should have_many :pet_applications}
+    it {should have_many(:pets).through(:pet_applications)}
   end
 end

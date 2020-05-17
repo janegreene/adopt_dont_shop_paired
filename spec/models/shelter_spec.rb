9c17fdf72dd4f030e9799ef37750e6c466babda1
Jane Greene <janegreenecasa@gmail.com>
2020-05-17 16:39:53 -0600

    completed user story 28 and updated shelter spec for pet destroy dependencies

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Shelter do
   describe 'validations' do
     it {should validate_presence_of :name}
   end
-  
-  describe 'relationships' do
-    it {should have_many :pets}
-    it {should have_many :reviews}
 
+  describe 'relationships' do
+    it {should have_many(:reviews).dependent(:destroy) }
+    it {should have_many(:pets).dependent(:destroy) }
   end
 end

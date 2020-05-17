7022d178884c2de9fb47cb40de7d670e346b6247
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-17 13:29:04 -0600

    complete story 22

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,27 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "the Application new page" do
 before(:each) do
-  @shelter1 = Shelter.create(name: "Find-a-Friend",
-    address: "123 North Street",
-    city: "Denver",
-    state: "CO",
-    zip: 80223 )
+    @shelter1 = Shelter.create(name: "Find-a-Friend",
+      address: "123 North Street",
+      city: "Denver",
+      state: "CO",
+      zip: 80223 )
     @pet1 = Pet.create(image: "https://ichef.bbci.co.uk/wwfeatures/live/976_549/images/live/p0/7z/n7/p07zn7p7.jpg",
       name: "Milo",
       age: "2",
       sex: "Male",
       shelter_id: @shelter1.id,
       description: "Small white dog",
-      status: "Adoptable"
-    )
+      status: "Adoptable")
+
     @pet2 = Pet.create(image: "https://ichef.bbci.co.uk/wwfeatures/live/976_549/images/live/p0/7z/n7/p07zn7p7.jpg",
       name: "Otis",
       age: "3",
       sex: "Male",
       shelter_id: @shelter1.id,
       description: "Small white dog",
-      status: "Adoptable"
-    )
+      status: "Adoptable")
+    
     @application = Application.create(name: "Will Rogers",
       address: "132 Maple Dr.", city: "Claremore", state: "OK", zip: 74014, phone: "918-233-9000",
       description: "great fenced yard", pet_ids: ["#{@pet1.id}", "#{@pet2.id}"])
@@ -51,7 +51,6 @@ before(:each) do
   end
     
     expect(current_path).to eq("/pets/#{@pet1.id}")
-
     expect(page).to have_content("Adoption Status: Pending")
     expect(page).to have_content("On hold for #{@application.name}.")
   end

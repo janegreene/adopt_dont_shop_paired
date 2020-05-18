afbd8d90365b9730567c67a4380e64a10e7cb0fc
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-17 20:55:37 -0600

    create shelter count of pets

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Shelter do
     it {should have_many(:reviews).dependent(:destroy) }
     it {should have_many(:pets).dependent(:destroy) }
   end
+
+  describe 'methods' do
+    shelter1 = Shelter.create(name: "Find-a-Friend",
+                              address: "123 North Street",
+                              city: "Denver",
+                              state: "CO",
+                              zip: 80223 )
+    it '#pet_count' do
+    expect(shelter1.pet_count).to eq(0)
+  end
+  end
 end
